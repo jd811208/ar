1fadfba2bb4df29dcbf53616fa4bce2963c413b1
jd811208 <jonas.dutra.lopes@escola.pr.gov.br>
2025-05-22 11:00:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,88 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    '',
+    '',
+    ''
+)
+
+criaCartao(
+    '',
+    '',
+    ''
+)
+
+criaCartao(
+    'Química',
+    'Qual é o ânodo da pilha?',
+    'O elemento químico com menor carga'
+)
+criaCartao(
+    'Sociologia',
+    'Qual era o material de estudo de Karl Marx?',
+    ' O materialismo histórico-dialético'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são as características dos peixes Ósseos?',
+    'Possuem esqueleto ósseo, brânqueas internas e apresentam uma linha lateral'
+)
+
+criaCartao(
+    'História',
+    'Qual era o objetivo de Hittler ao assinar o Acordo de Não-Agresão?',
+    'Era evitar uma guerra com a União Soviética após a invasão na polônia'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 8!?',
+    '40320'
+)
+
+criaCartao(
+    'Língua Portuguesa',
+    'Quais são as características de um conto?',
+    'Narrativa curta, poucos personagens e um espaço tempo menor'
+)
